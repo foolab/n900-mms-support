50435c6def002b98f270d9347471e24f664ef583
mohammed <mohammed@vader.localdomain>
2010-01-28 23:27:38 +0200

    Reworked the interface. We can now keep the status of the message

--- manager/folder.cc
+++ manager/folder.cc
@@ -2,32 +2,34 @@
 #include <QDir>
 #include <QDebug>
 #include <QTemporaryFile>
-#include "adaptor.hh"
+#include <QSettings>
 
 Folder::Folder(const QString& id, QObject *parent) :
-  QObject(parent), m_id(id), m_path(QString("/home/user/.mms/%1/").arg(id)) {
+  QObject(parent), m_id(id), m_path(QString("/home/user/.mms/%1").arg(id)) {
 
-  Adaptor *a = new Adaptor(this);
+  FolderAdaptor *a = new FolderAdaptor(this);
   Q_UNUSED(a);
 
-  QDir d(m_path);
+  QDir d(m_path + "/");
   if (!d.exists()) {
     if (!d.mkpath(".")) {
       qWarning() << "Failed to create" << m_path;
     }
   }
+
+  m_meta = new QSettings(m_path + ".meta", QSettings::IniFormat, this);
 }
 
 Folder::~Folder() {
 
 }
 
-QString Folder::folderId() {
-  return m_id;
+QString Folder::folderPath() {
+  return m_path + "/";
 }
 
-QString Folder::folderPath() {
-  return m_path;
+QString Folder::folderId() {
+  return m_id;
 }
 
 QString Folder::addMessage(const QByteArray& data) {
@@ -35,12 +37,13 @@ QString Folder::addMessage(const QByteArray& data) {
 
   f.setAutoRemove(false);
   if (!f.open()) {
-    qWarning() << "Failed to create a file" << f.errorString();
+    emitError(QString(), tr("Failed to create a file: %1").arg(f.errorString()));
     return QString();
   }
 
   if (f.write(data) != data.size()) {
-    qWarning() << "Failed to write to file" << f.fileName() << f.errorString();
+    emitError(QString(), tr("Failed to write to file %1: %2").arg(f.fileName()).arg(f.errorString()));
+    f.remove();
     return QString();
   }
 
@@ -48,42 +51,129 @@ QString Folder::addMessage(const QByteArray& data) {
 
   f.close();
 
-  qDebug() << "added message" << i.fileName();
+  //  qDebug() << "added message" << i.fileName();
+
+  setMessageStatus(i.fileName(), MESSAGE_STATUS_UNREAD);
 
   emit messageAdded(i.fileName());
 
   return i.fileName();
 }
 
+QString Folder::addMessage(const QString& fullPath) {
+  QFile f(fullPath);
+  if (!f.open(QFile::ReadOnly)) {
+    emitError(QString(), tr("Failed to open file %1: %2").arg(f.fileName()).arg(f.errorString()));
+    return false;
+  }
+
+  QByteArray data = f.readAll();
+  if (f.error() != QFile::NoError) {
+    emitError(QString(), tr("Failed to read from file %1: %2").arg(f.fileName()).arg(f.errorString()));
+    return false;
+  }
+
+  return addMessage(data);
+}
+
 bool Folder::deleteMessage(const QString& id) {
   QFile f(messageFileName(id));
 
   if (!f.exists()) {
-    qWarning() << "Message" << id << "does not exist.";
+    emitError(id, tr("Message does not exist"));
 
+    m_meta->remove(id);
     emit messageRemoved(id);
 
     return true;
   }
 
   if (!f.remove()) {
-    qWarning() << "Failed to remove" << id << f.errorString();
+    emitError(QString(), tr("Failed to remove file %1: %2").arg(f.fileName()).arg(f.errorString()));
     return false;
   }
 
+  m_meta->remove(id);
   emit messageRemoved(id);
 
   return true;
 }
 
 QStringList Folder::allMessages() {
-  QString lol("lll");
-
-  emit messageAdded(lol);
-
   return QDir(m_path).entryList(QDir::Files);
 }
 
 QString Folder::messageFileName(const QString& id) {
   return m_path + "/" + id;
+}
+
+int Folder::messageStatus(const QString& id) {
+  return m_meta->value(id + "/status").toInt();
+}
+
+bool Folder::setMessageStatus(const QString& id, const int& status) {
+  if (allMessages().indexOf(id) == -1) {
+    emitError(id, tr("Unknown message"));
+    return false;
+  }
+
+  if (status != messageStatus(id)) {
+    m_meta->setValue(id + "/status", status);
+    emit messageStatusChanged(id, status);
+  }
+
+  return true;
+}
+
+void Folder::emitError(const QString& id, const QString& message) {
+  QString er = QString("Folder %1: ").arg(folderId());
+  if (!id.isEmpty()) {
+    er.append(QString("(Message %1) ").arg(id));
+  }
+
+  er.append(message);
+
+  qWarning() << er;
+
+  emit error(id, message);
+}
+
+FolderAdaptor::FolderAdaptor(Folder *parent) : QDBusAbstractAdaptor(parent), m_folder(parent) {
+  setAutoRelaySignals(true);
+}
+
+FolderAdaptor::~FolderAdaptor() {
+
+}
+
+QString FolderAdaptor::folderId() {
+  return m_folder->folderId();
+}
+
+QString FolderAdaptor::addMessage(const QByteArray& data) {
+  return m_folder->addMessage(data);
+}
+
+QString FolderAdaptor::addMessage(const QString& fullPath) {
+  return m_folder->addMessage(fullPath);
+}
+
+bool FolderAdaptor::deleteMessage(const QString& id) {
+  return m_folder->deleteMessage(id);
+}
+
+QStringList FolderAdaptor::allMessages() {
+  return m_folder->allMessages();
+}
+
+QString FolderAdaptor::messageFileName(const QString& id) {
+  return m_folder->messageFileName(id);
+}
+
+int FolderAdaptor::messageStatus(const QString& id) {
+  return m_folder->messageStatus(id);
+}
+
+bool FolderAdaptor::setMessageStatus(const QString& id, const int& status) {
+  return m_folder->setMessageStatus(id, status);
 }
--- manager/folder.hh
+++ manager/folder.hh
@@ -3,6 +3,16 @@
 
 #include <QObject>
 #include <QStringList>
+#include <QDBusAbstractAdaptor>
+
+#define MESSAGE_STATUS_UNKNOWN 0
+#define MESSAGE_STATUS_SENT 1
+#define MESSAGE_STATUS_SENDING 2
+#define MESSAGE_STATUS_FAILED 3
+#define MESSAGE_STATUS_READ 4
+#define MESSAGE_STATUS_UNREAD 5
+
+class QSettings;
 
 class Folder : public QObject {
   Q_OBJECT
@@ -11,23 +21,68 @@ public:
   Folder(const QString& id, QObject *parent = 0);
   virtual ~Folder();
 
-  QString folderId();
   QString folderPath();
 
 signals:
   void messageAdded(const QString& id);
   void messageRemoved(const QString& id);
+  void messageStatusChanged(const QString& id, const int status);
+  void error(const QString& id, const QString& message);
 
 public slots:
+  QString folderId();
+
   QString addMessage(const QByteArray& data);
+  QString addMessage(const QString& fullPath);
+
   bool deleteMessage(const QString& id);
 
   QStringList allMessages();
   QString messageFileName(const QString& id);
 
+  int messageStatus(const QString& id);
+
+  bool setMessageStatus(const QString& id, const int& status);
+
 private:
+  void emitError(const QString& id, const QString& message);
+
   const QString m_id;
   const QString m_path;
+  QSettings *m_meta;
+};
+
+class FolderAdaptor : public QDBusAbstractAdaptor {
+  Q_OBJECT
+  Q_CLASSINFO("D-Bus Interface", "org.foolab.MMS.Folder");
+
+public:
+  FolderAdaptor(Folder *parent);
+  ~FolderAdaptor();
+
+signals:
+  void messageAdded(const QString& id);
+  void messageRemoved(const QString& id);
+  void messageStatusChanged(const QString& id, const int status);
+  void error(const QString& id, const QString& message);
+
+public slots:
+  QString folderId();
+
+  QString addMessage(const QByteArray& data);
+  QString addMessage(const QString& fullPath);
+
+  bool deleteMessage(const QString& id);
+
+  QStringList allMessages();
+  QString messageFileName(const QString& id);
+
+  int messageStatus(const QString& id);
+
+  bool setMessageStatus(const QString& id, const int& status);
+
+private:
+  Folder *m_folder;
 };
 
 #endif /* FOLDER_HH */
