d71e56801253ace9cb39e35d4b8726a5c2af1288
Mohammed Sameer <msameer@foolab.org>
2010-03-14 22:04:51 +0200

    Implemented the 2 HandleWAPPush mesthods. We now have a separate class for the adaptor

--- manager/wappush.cc
+++ manager/wappush.cc
@@ -1,8 +1,9 @@
 #include "wappush.hh"
 #include <QDebug>
 
-WapPush::WapPush(QObject *parent) : QDBusAbstractAdaptor(parent) {
-
+WapPush::WapPush(QObject *parent) : QObject(parent) {
+  WapPushAdaptor *adaptor = new WapPushAdaptor(this);
+  Q_UNUSED(adaptor);
 }
 
 WapPush::~WapPush() {
@@ -18,5 +19,41 @@ void WapPush::HandleWAPPush(const uchar& bearer, const QString& source, const in
   Q_UNUSED(dst);
   Q_UNUSED(header);
 
+  qDebug() << "WapPush::HandleWAPPush";
+
   emit addMessage(payload);
+}
+
+void WapPush::HandleWAPPush(const uchar& bearer, const QByteArray& src, const QByteArray& dst,
+			    const int sport, const int dport, const QByteArray& header, const QByteArray& payload) {
+
+  Q_UNUSED(bearer);
+  Q_UNUSED(src);
+  Q_UNUSED(dst);
+  Q_UNUSED(sport);
+  Q_UNUSED(dport);
+  Q_UNUSED(header);
+  Q_UNUSED(payload);
+
+  qDebug() << "WapPush::HandleWAPPush2";
+}
+
+
+
+WapPushAdaptor::WapPushAdaptor(WapPush *parent) : QDBusAbstractAdaptor(parent), m_push(parent) {
+
+}
+
+WapPushAdaptor::~WapPushAdaptor() {
+
+}
+
+void WapPushAdaptor::HandleWAPPush(const uchar& bearer, const QString& source, const int src, const int dst,
+				   const QByteArray& header, const QByteArray& payload) {
+  m_push->HandleWAPPush(bearer, source, src, dst, header, payload);
+}
+
+void WapPushAdaptor::HandleWAPPush(const uchar& bearer, const QByteArray& src, const QByteArray& dst,
+				   const int sport, const int dport, const QByteArray& header, const QByteArray& payload) {
+  m_push->HandleWAPPush(bearer, src, dst, sport, dport, header, payload);
 }
--- manager/wappush.hh
+++ manager/wappush.hh
@@ -1,11 +1,11 @@
 #ifndef WAP_PUSH_HH
 #define WAP_PUSH_HH
 
+#include <QObject>
 #include <QDBusAbstractAdaptor>
 
-class WapPush : public QDBusAbstractAdaptor {
+class WapPush : public QObject {
   Q_OBJECT
-  Q_CLASSINFO("D-Bus Interface", "com.nokia.WAPPushHandler");
 
 public:
   WapPush(QObject *parent = 0);
@@ -15,8 +15,30 @@ signals:
   void addMessage(const QByteArray& message);
 
 public slots:
-   Q_NOREPLY void HandleWAPPush(const uchar& bearer, const QString& source, const int src, const int dst,
-				const QByteArray& header, const QByteArray& payload);
+  Q_NOREPLY void HandleWAPPush(const uchar& bearer, const QString& source, const int src, const int dst,
+			       const QByteArray& header, const QByteArray& payload);
+
+  Q_NOREPLY void HandleWAPPush(const uchar& bearer, const QByteArray& src, const QByteArray& dst,
+			       const int sport, const int dport, const QByteArray& header, const QByteArray& payload);
+};
+
+class WapPushAdaptor : public QDBusAbstractAdaptor {
+  Q_OBJECT
+  Q_CLASSINFO("D-Bus Interface", "com.nokia.WAPPushHandler");
+
+public:
+  WapPushAdaptor(WapPush *parent);
+  ~WapPushAdaptor();
+
+public slots:
+  Q_NOREPLY void HandleWAPPush(const uchar& bearer, const QString& source, const int src, const int dst,
+			       const QByteArray& header, const QByteArray& payload);
+
+  Q_NOREPLY void HandleWAPPush(const uchar& bearer, const QByteArray& src, const QByteArray& dst,
+			       const int sport, const int dport, const QByteArray& header, const QByteArray& payload);
+
+private:
+  WapPush *m_push;
 };
 
 #endif /* WAP_PUSH_HH */
